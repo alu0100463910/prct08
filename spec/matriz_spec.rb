0f248ac68b9d408bafddef32800f756248be7bb3
Fabio Mendoza Bello <alu0100463910@ull.edu.es>
2013-11-08 15:05:56 +0000

    auto upload by gitpusher

--- spec/matriz_spec.rb
+++ spec/matriz_spec.rb
@@ -22,12 +22,12 @@ describe Matriz do
   
   it "Se debe poder sumar dos matrices de enteros" do
     (@m1+@m3).matrix.inspect.should == "[[4, 5, 6], [7, 8, 9]]"
-    (@matriz1+@matriz2).matrix.inspect.should == "[[2,3], [4,2]]"
+    (@matriz1+@matriz2).matrix.inspect.should == "[[2, 3], [4, 2]]"
   end
   
   it "Se debe poder multiplicar una matriz de Enteros" do
     (@m1*@m2).matrix.inspect.should == "[[18, 18, 18], [45, 45, 45]]"
-    (@matriz1*@matriz2).matrix.inspect.should == "[[3/4,9/4],[15/4,3/4]]"
+    (@matriz1*@matriz2).matrix.inspect.should == "[[3/4, 9/4], [15/4, 3/4]]"
   end
   
 end
